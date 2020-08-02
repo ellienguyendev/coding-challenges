05f10c12814d6631c94c9d0cda4d93e53e79cd66
Ellie Nguyen <ellienguyendev@gmail.com>
2020-08-01 20:25:20 -0700

    08.01

--- 0731-to-0808.js
+++ 0731-to-0808.js
@@ -1,4 +1,5 @@
-// Day 1: 07.31.2020
+// ***** Day 1: 07.31.2020 6kyu kata*****
+//https://www.codewars.com/kata/54129112fb7c188740000162/train/javascript
 // Create the function prefill that returns an array of n elements that all have the same v v. See if you can do this without using a loop.
 // You have to validate input:
 
@@ -32,3 +33,29 @@ function prefill(n, v) {
 }
 
 prefill (3, "abcde")
+
+// ***** Day 2: 08.01.2020 6kyu kata*****
+// https://www.codewars.com/kata/554a44516729e4d80b000012/train/javascript
+// A man has a rather old car being worth $2000. He saw a secondhand car being worth $8000. He wants to keep his old car until he can buy the secondhand one.
+// He thinks he can save $1000 each month but the prices of his old car and of the new one decrease of 1.5 percent per month. Furthermore this percent of loss increases of 0.5 percent at the end of every two months. Our man finds it difficult to make all these calculations.
+// Can you help him? How many months will it take him to save up enough money to buy the car he wants, and how much money will he have left over?
+
+function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
+  var totalSavings = 0
+  for(let month=0 ; month <= 12; month++){
+    if (totalSavings + startPriceOld <= startPriceNew){
+      if (month % 2 === 0 && month > 1){
+        percentLossByMonth += 0.5
+      }
+      totalSavings += savingperMonth
+      startPriceOld = startPriceOld - Math.floor(startPriceOld * (percentLossByMonth/100))
+      startPriceNew = startPriceNew - Math.floor(startPriceNew * (percentLossByMonth/100))
+    } else {
+    console.log([month, (totalSavings + startPriceOld) - startPriceNew])
+    break
+  }
+  }
+}
+
+nbMonths(2000, 8000, 1000, 1.5)
+// **Solution not 100% complete yet**
