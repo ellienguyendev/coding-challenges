1a4870a267a34ebf4674cd7af75d89ff91f08529
Ellie Nguyen <ellienguyendev@gmail.com>
2020-08-08 22:50:47 -0700

    08.08 6kyu kata

--- 0731-to-0808.js
+++ 0731-to-0808.js
@@ -1,4 +1,28 @@
 // ***** Day 8: 08.07.2020 6kyu kata*****
+// In this Kata, we are going to reverse a string while maintaining the spaces (if any) in their original place.
+// For example:
+// solve("our code") = "edo cruo"
+// -- Normal reversal without spaces is "edocruo".
+// -- However, there is a space after the first three characters, hence "edo cruo"
+// solve("your code rocks") = "skco redo cruoy"
+// solve("codewars") = "srawedoc"
+
+function reverse(str){
+  var arr = str.split('')
+  var indexes = []
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] === ' '){
+      indexes.push(i)
+      console.log(indexes)
+    }
+  }
+  str = arr.reverse().join('')
+  console.log(str)
+}
+
+reverse('hello puppy i love you')
+
+// ***** Day 8: 08.07.2020 6kyu kata*****
 // Complete the solution so that the function will break up camel casing, using a space between words.
 // Example: solution("camelCasing")  ==  "camel Casing"
 
