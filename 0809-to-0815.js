7663bfc4b5dc311f3ec923fd599a94851fc2c8ca
Ellie Nguyen <ellienguyendev@gmail.com>
2020-08-09 19:20:29 -0700

    08.09 Leetcode

---
+++ 0809-to-0815.js
@@ -0,0 +1,18 @@
+// Day 10 08.09.2020 Leetcode 1512. Number of Good Pairs
+// Given an array of integers nums.
+// A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+// Return the number of good pairs.
+
+var numIdenticalPairs = function(nums) {
+var goodPairs = []
+
+for(let i=0; i<nums.length; i++){
+  for(let j=i+1; j<nums.length; j++)
+    if(nums[i]===nums[j] && i < j){
+      goodPairs.push([i,j])
+    }
+}
+    return goodPairs.length
+};
+
+numIdenticalPairs([1,2,3,1,1,3])
