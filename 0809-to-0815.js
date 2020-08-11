3ab7f8a57ffdab7f954dbbc0ddeecaacae9274e1
Ellie Nguyen <51030761+ellienguyendev@users.noreply.github.com>
2020-08-10 20:08:58 -0700

    Update 0809-to-0815.js

--- 0809-to-0815.js
+++ 0809-to-0815.js
@@ -1,3 +1,17 @@
+// Day 11 08.10.2020 Leetcode Single Number
+// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+// Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+// Example 1: Input: [2,2,1] => Output: 1
+
+var singleNumber = function(nums) {
+  var single
+  for (var i = 0; i < nums.length; i++) {
+    if (nums.indexOf(nums[i], nums.indexOf(nums[i]) + 1) == -1)
+      single = nums[i]
+  }
+  return single
+}
+
 // Day 10 08.09.2020 Leetcode 1512. Number of Good Pairs
 // Given an array of integers nums.
 // A pair (i,j) is called good if nums[i] == nums[j] and i < j.
