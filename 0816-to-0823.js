017d3db42fb9209a784cef3a5cd0f96569e37d12
Ellie Nguyen <ellienguyendev@gmail.com>
2020-08-17 21:12:44 -0700

    08.16 Leetcode

--- 0816-to-0823.js
+++ 0816-to-0823.js
@@ -1,3 +1,16 @@
+// Day 18 08.17.2020 Leetcode Single Number
+// Given an array of integers, find if the array contains any duplicates.
+// Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+var containsDuplicate = function(nums) {
+  var unique = nums.filter(x => nums.indexOf(x) === nums.lastIndexOf(x))
+  if (unique.length <= nums.length - 1){
+    return true
+  } else {
+    return false
+  }
+};
+
 // Day 17 08.16.2020 Leetcode Single Number
 // Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 // Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
