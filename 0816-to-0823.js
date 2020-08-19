ede4a25ab3e80ad2a8392d9b08e345817c9833f8
Ellie Nguyen <ellienguyendev@gmail.com>
2020-08-18 21:35:08 -0700

    08.18 Leetcode

--- 0816-to-0823.js
+++ 0816-to-0823.js
@@ -1,3 +1,17 @@
+// Day 19 08.18.2020 Leetcode Plus One
+// Given a non-empty array of digits representing a non-negative integer, increment one to the integer.
+// The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+// You may assume the integer does not contain any leading zero, except the number 0 itself.
+// Example 1:
+// Input: [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+
+var plusOne = function(digits) {
+  var num = parseFloat(digits.join('')) + 1
+  return num.split('')
+};
+
 // Day 18 08.17.2020 Leetcode Contains Duplicate
 // Given an array of integers, find if the array contains any duplicates.
 // Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
