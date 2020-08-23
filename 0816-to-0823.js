470354b6ed1c1b808b21fc49f9381f91bb40d1d8
Ellie Nguyen <ellienguyendev@gmail.com>
2020-08-22 19:35:00 -0700

    08.22 6kyu kata

--- 0816-to-0823.js
+++ 0816-to-0823.js
@@ -1,3 +1,29 @@
+// Day 23 08.22.2020 Codewar 6kyu Kata
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+// Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
+// likes [] // must be "no one likes this"
+// likes ["Peter"] // must be "Peter likes this"
+// likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+// likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+// likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+
+function likes(names) {
+  var length = names.length
+  var display
+  if(length === 0){
+    display = "no one likes this"
+  } else if(length === 1){
+    display = `${names[0]} likes this`
+  } else if(length === 2){
+    display = `${names[0]} and ${names[1]} like this`
+  } else if(length === 3){
+    display = `${names[0]}, ${names[1]} and ${names[2]} like this`
+  } else {
+    display = `${names[0]}, ${names[1]} and ${length-2} others like this`
+  }
+  return display
+}
+
 // Day 21 08.20.2020 Codewar 7kyu Kata
 // Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
 
