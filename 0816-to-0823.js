d37cf65b66d579ecacc1c36acbdb089f777a5ad9
Ellie Nguyen <ellienguyendev@gmail.com>
2020-08-23 17:45:03 -0700

    08.23

--- 0816-to-0823.js
+++ 0816-to-0823.js
@@ -1,5 +1,4 @@
 // Day 23 08.22.2020 Codewar 6kyu Kata
-// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
 // Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
 // likes [] // must be "no one likes this"
 // likes ["Peter"] // must be "Peter likes this"
