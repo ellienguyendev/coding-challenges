c9940b07183184b6d4ed509746e504977cb35ecc
Ellie Nguyen <ellienguyendev@gmail.com>
2020-08-27 15:01:36 -0700

    08.27 leetcode

--- 0819-Leetcode-Rotate-Array.py
+++ 0819-Leetcode-Rotate-Array.py
@@ -1,11 +1,9 @@
 # Day 20 08.19.2020 Leetcode RotateArray
 # Given an array, rotate the array to the right by k steps, where k is non-negative.
 
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        a = [0] * n
-        for i in range(n):
-            a[(i + k) % n] = nums[i]
-
-        nums[:] = a
+class Solution(object):
+    def moveZeroes(self, nums):
+        append_times = nums.count(0)
+        for i in range(append_times):
+            nums.remove(0) #  Delete the front zero
+            nums.append(0) # append it at the end of nums, the times of the addition and substraction shall be equal
