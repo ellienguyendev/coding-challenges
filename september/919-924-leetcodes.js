ed6b92ada11897da3f83526819e3b3867d6a7d4a
Ellie Nguyen <ellienguyendev@gmail.com>
2020-09-20 20:00:00 -0600

    9.20 leetcode

--- september/919-924-leetcodes.js
+++ september/919-924-leetcodes.js
@@ -15,3 +15,30 @@ var maxProfit = function(prices) {
     }
     return profit;
 };
+
+// 9.20.2020 Leetcode Cells with Odd Values in a Matrix
+// https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/
+
+/**
+ * @param {number} n
+ * @param {number} m
+ * @param {number[][]} indices
+ * @return {number}
+ */
+var oddCells = function (n, m, indices) {
+  const row = new Array(n).fill(0)
+  const col = new Array(m).fill(0)
+  for (let i = 0; i < indices.length; i++) {
+    row[indices[i][0]]++;
+    col[indices[i][1]]++;
+  }
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if ((row[i] + col[j]) % 2 !== 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
