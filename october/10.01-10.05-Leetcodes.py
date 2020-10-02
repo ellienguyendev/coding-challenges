85890440854c33e46a09b96d33949775ff0e0b62
Ellie Nguyen <ellienguyendev@gmail.com>
2020-10-01 20:00:00 -0600

    1001 leetcode subdomain visit

---
+++ october/10.01-10.05-Leetcodes.py
@@ -0,0 +1,23 @@
+# 10.01.2020 Subdomain Visit
+# https://leetcode.com/problems/subdomain-visit-count/
+
+/**
+ * @param {string[]} cpdomains
+ * @return {string[]}
+ */
+
+var subdomainVisits = function(cpdomains) {
+  let map = new Map();
+  let result = [];
+  cpdomains.map(item => {
+    let [count, domains] = item.split(" ");
+    let fromIndex = 0;
+    while (fromIndex != -1) {
+      let subDomain = domains.slice(fromIndex + (fromIndex != 0 ? 1 : 0));
+      map.get(subDomain) ? map.set(subDomain, map.get(subDomain) + +count) : map.set(subDomain, +count);
+      fromIndex = domains.indexOf(".", fromIndex + 1);
+    }
+  })
+  map.forEach((v, k) => result.push(v + " " + k))
+  return result;
+}
