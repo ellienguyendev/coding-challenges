bf09cfa1d17367f6e2b8bdea3defe0bcc2b653c9
Ellie Nguyen <ellienguyendev@gmail.com>
2020-10-03 20:00:00 -0600

    1003 leetcode Split a String in Balanced Strings

---
+++ october/10.01-10.05-Leetcodes.js
@@ -0,0 +1,28 @@
+// 10.01.2020 Subdomain Visit
+// https://leetcode.com/problems/subdomain-visit-count/
+
+/**
+ * @param {string[]} cpdomains
+ * @return {string[]}
+ */
+
+var subdomainVisits = function(cpdomains) {
+  let map = new Map();
+  let result = [];
+  cpdomains.map(item => {
+    let [count, domains] = item.split(" ");
+    let fromIndex = 0;
+    while (fromIndex != -1) {
+      let subDomain = domains.slice(fromIndex + (fromIndex != 0 ? 1 : 0));
+      map.get(subDomain) ? map.set(subDomain, map.get(subDomain) + +count) : map.set(subDomain, +count);
+      fromIndex = domains.indexOf(".", fromIndex + 1);
+    }
+  })
+  map.forEach((v, k) => result.push(v + " " + k))
+  return result;
+}
+
+// 10.02.2020 Swap
+// https://leetcode.com/problems/swap-salary/
+
+UPDATE salary SET sex = IF(sex='m', 'f', 'm');
