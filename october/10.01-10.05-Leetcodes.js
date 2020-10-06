1640bc1c7c2052004a0f828addfc974cb571fd12
Ellie Nguyen <ellienguyendev@gmail.com>
2020-10-06 15:29:15 -0700

    1004 leetcode Sort Array By Parity

--- october/10.01-10.05-Leetcodes.js
+++ october/10.01-10.05-Leetcodes.js
@@ -26,3 +26,44 @@ var subdomainVisits = function(cpdomains) {
 // https://leetcode.com/problems/swap-salary/
 
 UPDATE salary SET sex = IF(sex='m', 'f', 'm');
+
+
+// 10.03.2020 Split a String in Balanced Strings
+// https://leetcode.com/problems/split-a-string-in-balanced-strings/submissions/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+
+    let numOfEqualStrings = 0;
+    let currentBalance = 0;
+
+    for(let i = 0; i <s.length; i++){
+        const letter = s[i]
+
+        letter === 'L'? currentBalance++ : currentBalance--;
+
+        if(currentBalance === 0) numOfEqualStrings++
+    }
+
+    return numOfEqualStrings
+
+};
+
+// 10.04.2020 Sort Array By Parity
+//https://leetcode.com/problems/sort-array-by-parity/
+
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortArrayByParity = function(A) {
+    const even = [];
+    const odd = [];
+
+    A.forEach(num => num % 2 === 0 ? even.push(num) : odd.push(num) )
+
+    return [...even, ...odd]
+};
